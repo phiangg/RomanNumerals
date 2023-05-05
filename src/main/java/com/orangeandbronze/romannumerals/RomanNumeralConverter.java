dbd78f4a56ad0cbef88f120b72b827665b0323f5
Phiangg <sophiaquizon1102@gmail.com>
2023-05-05 13:51:15 +0800

    Test with 4

--- src/main/java/com/orangeandbronze/romannumerals/RomanNumeralConverter.java
+++ src/main/java/com/orangeandbronze/romannumerals/RomanNumeralConverter.java
@@ -1,2 +1,65 @@
-package com.orangeandbronze.romannumerals;public class RomanNumeralConverter {
+package com.orangeandbronze.romannumerals;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class RomanNumeralConverter {
+
+    //int to roman string
+
+    //method signature
+    String intToRoman(int num) {
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            builder.append("I");
+        }
+        return builder.toString();
+
+    }
 }
+
+/*if(num == 2){
+            return "II";
+        }else {
+            return "I";
+        }*/
+
+//return num == 2 ? "II" : "I";
+
+//....
+
+/*LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
+        roman_numerals.put("I", 1);
+        roman_numerals.put("IV", 4);
+        roman_numerals.put("V", 5);
+        roman_numerals.put("IX", 9);
+        roman_numerals.put("X", 10);
+        roman_numerals.put("XL", 40);
+        roman_numerals.put("L", 50);
+        roman_numerals.put("M", 1000);
+        roman_numerals.put("CM", 900);
+        roman_numerals.put("D", 500);
+        roman_numerals.put("CD", 400);
+        roman_numerals.put("C", 100);
+        roman_numerals.put("XC", 90);
+
+        String res = "";
+
+        for (Map.Entry<String, Integer> entry : roman_numerals.entrySet()){
+            int matches = num/entry.getValue();
+            res += repeat(entry.getKey(), matches);
+            num = num % entry.getValue();
+        }
+        return res;
+    }
+
+    public static String repeat(String s, int n){
+        if (s == null){
+            return null;
+        } final StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            sb.append(s);
+        }
+        return sb.toString();
+    }*/
--- src/test/java/com/orangeandbronze/romannumerals/RomanNumeralConverterTest.java
+++ src/test/java/com/orangeandbronze/romannumerals/RomanNumeralConverterTest.java
@@ -1,2 +1,41 @@
-package com.orangeandbronze.romannumerals;public class RomanNumeralConverterTest {
+package com.orangeandbronze.romannumerals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RomanNumeralConverterTest {
+    @Test
+    void intToRoman_1() {
+        //Given argument of 1
+        int num = 1;
+        //When argument is passed into parameter of method
+        String actual = new RomanNumeralConverter().intToRoman(num);
+        //Then method should return "I"
+        assertEquals("I", actual);
+    }
+
+    @Test
+    void intToRoman_2() {
+        assertEquals("II", new RomanNumeralConverter().intToRoman(2));
+    }
+
+    @Test
+    void intToRoman_3() {
+        assertEquals("III", new RomanNumeralConverter().intToRoman(3));
+    }
+
+    @Test
+    void intToRoman_4() {
+        assertEquals("IV", new RomanNumeralConverter().intToRoman(4));
+    }
+
+    @Test
+    void intToRoman_5() {
+        assertEquals("V", new RomanNumeralConverter().intToRoman(5));
+    }
+
+    @Test
+    void intToRoman_10() {
+        assertEquals("X", new RomanNumeralConverter().intToRoman(10));
+    }
 }
